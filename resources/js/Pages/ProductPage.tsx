e3177b2faaa4e1f1b92836d7b67336e771a76475
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-01-06 15:06:39 +0700

    Create ProductPage.tsx

---
+++ resources/js/Pages/ProductPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProductContent from "@/components/ProductContent";
+
+const ProductPage: React.FC = () => {
+  return (
+      
+      <ProductContent />
+  );
+};
+
+export default ProductPage;
