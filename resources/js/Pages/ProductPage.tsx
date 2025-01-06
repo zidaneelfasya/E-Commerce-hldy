556b05d83feb5d6cae7b581ccbf34ff8b5a89f13
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-01-06 15:23:43 +0700

    Update ProductPage.tsx

--- resources/js/Pages/ProductPage.tsx
+++ resources/js/Pages/ProductPage.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ProductContent from "@/components/ProductContent";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const ProductPage: React.FC = () => {
-  return (
-      
-      <ProductContent />
-  );
+    return (
+        <div>
+            <Header />
+            <main className="pt-16">
+                <ProductContent />
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default ProductPage;
