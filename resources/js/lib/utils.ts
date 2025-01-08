513fefc337bde9b31513393771435d3880471f83
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-01-08 20:25:04 +0700

    add variabel api axios

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const api = axios.create({
+    baseURL: "http://127.0.0.1:8000/api",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+});
+
+export default api;
