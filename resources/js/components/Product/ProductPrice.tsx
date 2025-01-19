20bf0dad1a525fd6b6a36f4dd5fcc96d8542ffa6
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-01-19 22:19:33 +0700

    Update ProductPrice.tsx

--- resources/js/components/Product/ProductPrice.tsx
+++ resources/js/components/Product/ProductPrice.tsx
@@ -3,14 +3,17 @@ import React from "react";
 interface ProductPriceProps {
   price: number;
   originalPrice: number;
+  discPrice: number;
   discount: number;
+
 }
 
-const ProductPrice: React.FC<ProductPriceProps> = ({ price, originalPrice, discount }) => {
+const ProductPrice: React.FC<ProductPriceProps> = ({ price, originalPrice, discPrice, discount }) => {
   return (
+    
     <div className="flex items-center gap-4 mb-4">
       <p className="text-purple-600 text-2xl font-bold">
-        Rp {price.toLocaleString("id-ID")}
+        Rp {discPrice.toLocaleString("id-ID")}
       </p>
       <p className="text-gray-500 line-through">
         Rp {originalPrice.toLocaleString("id-ID")}
