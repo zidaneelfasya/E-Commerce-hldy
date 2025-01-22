db6f2bbd464039d2cceb930181ad1777bc62f1a6
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-01-22 21:15:20 +0700

    Update ProductCard.tsx

--- resources/js/components/Product/ProductCard.tsx
+++ resources/js/components/Product/ProductCard.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 interface ProductCardProps {
-    image: string;
+    image_path: string;
     name: string;
     oldPrice: string;
     newPrice: string;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({
-    image,
+    image_path,
     name,
     oldPrice,
     newPrice,
 }) => {
     return (
         <div className="border-solid p-2.5 w-80 text-left transition-opacity duration-300 ease-in-out hover:opacity-75">
-            <img src={image} alt={name} className="product-image" />
+            <img src={`/storage/${image_path}`} alt={name} className="product-image" />
             <h3 className="font-extrabold text-lg">{name}</h3>
             <div className="flex justify-between">
                 <span className="line-through">{oldPrice}</span>
