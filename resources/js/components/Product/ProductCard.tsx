5e7233b4802252f5581ca3cad8996e400cd0e280
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-01-24 19:18:51 +0700

    Update ProductCard.tsx

--- resources/js/components/Product/ProductCard.tsx
+++ resources/js/components/Product/ProductCard.tsx
@@ -3,8 +3,8 @@ import React from "react";
 interface ProductCardProps {
     image_path: string; // Single image URL
     name: string;
-    oldPrice: string;
-    newPrice: string;
+    oldPrice: number;
+    newPrice: number;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({
@@ -13,16 +13,32 @@ const ProductCard: React.FC<ProductCardProps> = ({
     oldPrice,
     newPrice,
 }) => {
-    return (
-        <div className="border-solid p-2.5 w-80 text-left transition-opacity duration-300 ease-in-out hover:opacity-75">
-            <img src={`/storage/${image_path}`} alt={name} className="product-image" />
-            <h3 className="font-extrabold text-lg">{name}</h3>
-            <div className="flex justify-between">
-                <span className="line-through">{oldPrice}</span>
-                <span className="text-bold">{newPrice}</span>
+    if(newPrice == 0){
+        return (
+            <div className="border-solid p-2.5 w-80 text-left transition-opacity duration-300 ease-in-out hover:opacity-75">
+                <img src={`/storage/${image_path}`} alt={name} className="product-image" />
+                <h3 className="font-extrabold text-lg">{name}</h3>
+    
+                <div className="flex justify-between">
+                    <span className="text-bold">{oldPrice}</span>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    if(newPrice != 0){
+        return (
+            <div className="border-solid p-2.5 w-80 text-left transition-opacity duration-300 ease-in-out hover:opacity-75">
+                <img src={`/storage/${image_path}`} alt={name} className="product-image" />
+                <h3 className="font-extrabold text-lg">{name}</h3>
+    
+                <div className="flex justify-between">
+                    <span className="line-through">{oldPrice}</span>
+                    <span className="text-bold">{newPrice}</span>
+                </div>
+            </div>
+        );
+    }
+    
 };
 
 export default ProductCard;
