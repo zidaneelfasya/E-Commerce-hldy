e79ae750b0b7e604c335df07e1ae1eb765b1b01c
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-02-13 21:19:19 +0700

    Update Welcome.tsx

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -8,6 +8,7 @@ import { ComboboxDemo } from "@/components/ComboboxDemo";
 import { Search } from "lucide-react";
 import ProductCard from "@/components/ProductCard";
 import AccordionDemo from "@/components/accordion";
+import Header from "@/components/Header";
 
 const categories = [
     { value: "electronics", label: "Electronics" },
@@ -27,40 +28,7 @@ const Welcome = () => {
     return (
         <div>
             {/* Header */}
-            <header className="bg-purple-800 text-white py-4 fixed top-0 w-full z-10">
-                <div className="container mx-auto flex justify-between items-center">
-                    <div className="text-lg font-bold">hldy</div>
-                    <nav className="flex space-x-4">
-                        <a href="#" className="hover:underline">
-                            Home
-                        </a>
-                        <a href="#" className="hover:underline">
-                            Shop
-                        </a>
-                        <a href="#" className="hover:underline">
-                            Cart
-                        </a>
-                        <a href="#" className="hover:underline">
-                            Contact
-                        </a>
-                    </nav>
-                    <div className="flex w-full max-w-lg items-center space-x-2">
-                        <Input
-                            type="email"
-                            className="border p-2 w-3/4 bg-white"
-                            placeholder="Cari Produk"
-                        />
-                        <Button type="submit" className="w-1/4" variant="ghost">
-                            <Search />
-                        </Button>
-                    </div>
-                    <div className="flex space-x-4 mt-4">
-                        <Button variant="ghost">Login</Button>
-                        <Button variant="ghost">Register</Button>
-                    </div>
-                </div>
-            </header>
-
+            <Header />
             {/* Hero Section */}
             <section className="bg-gray-100 text-center py-16">
                 <div className="slider">
